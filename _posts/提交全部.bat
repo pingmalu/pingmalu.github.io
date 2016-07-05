6a6b814b356089c61a95493a08f359018dc7480f
pingmalu <malu@malu.me>
2016-07-05 15:17:23 +0800

    'update'

---
+++ _posts/提交全部.bat
@@ -0,0 +1,7 @@
+@echo off
+@echo ========================================================
+@echo Ã·Ωªgit
+@echo ========================================================
+
+C:\Users\PHP-Wangxl\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\git-cmd.exe "git add . && git commit -am 'update' && git push"
+exit
---
+++ _posts/更新全部目录.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo ========================================================
+@echo ¸üÐÂgit
+@echo ========================================================
+C:\Users\PHP-Wangxl\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\git-cmd.exe git pull & exit
