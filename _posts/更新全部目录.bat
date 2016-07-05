4821185447bb788e52d91af2c5ab3d3c3dc10c70
pingmalu <malu@malu.me>
2016-07-05 16:01:07 +0800

    'update'

--- _posts/更新全部目录.bat
+++ _posts/更新全部目录.bat
@@ -2,4 +2,4 @@
 @echo ========================================================
 @echo ¸üÐÂgit
 @echo ========================================================
-C:\Users\PHP-Wangxl\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\git-cmd.exe git pull & exit
+C:\Users\PHP-Wangxl\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\git-cmd.exe "git pull && exit"
