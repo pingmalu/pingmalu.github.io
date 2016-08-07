0f10b5aa2e5c64f52eace72fef5f6108a8f3db0e
malu <malu@malu.me>
2016-08-07 16:04:12 +0800

    'update'

---
+++ _posts/dell更新全部目录.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo ========================================================
+@echo ¸üÐÂgit
+@echo ========================================================
+C:\Users\PRO11\AppData\Local\GitHub\PortableGit_25d850739bc178b2eb13c3e2a9faafea2f9143c0\git-cmd.exe "git pull && exit"
