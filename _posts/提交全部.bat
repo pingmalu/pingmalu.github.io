c1e3697468926d4f7f054a7df22fef0546647367
malu <malu@malu.me>
2019-10-15 09:54:21 +0800

    'update'

--- _posts/提交全部.bat
+++ _posts/提交全部.bat
@@ -3,5 +3,5 @@
 @echo Ã·Ωªgit
 @echo ========================================================
 
-C:\Users\PHP-Wangxl\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\git-cmd.exe "git add . && git commit -am 'update' && git push && exit"
+"C:\Program Files\Git\git-cmd.exe" "git add . && git commit -am 'update' && git push && exit"
 exit
