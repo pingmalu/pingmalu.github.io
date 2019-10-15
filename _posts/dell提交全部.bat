aefc4f87a9f1fd21e7fd0b0e9ec85f906c248233
malu <malu@malu.me>
2019-10-15 10:04:44 +0800

    'update'

--- _posts/dell提交全部.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@echo ========================================================
-@echo Ã·Ωªgit
-@echo ========================================================
-
-C:\Users\PRO11\AppData\Local\GitHub\PortableGit_25d850739bc178b2eb13c3e2a9faafea2f9143c0\git-cmd.exe "git add . && git commit -am 'update' && git push && exit"
-exit
--- _posts/更新全部目录.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@echo ========================================================
-@echo ¸üÐÂgit
-@echo ========================================================
-C:\Users\PHP-Wangxl\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\git-cmd.exe "git pull && exit"
