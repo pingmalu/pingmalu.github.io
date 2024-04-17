cc0fe27a594adedcf66ac9c3382dff68dd83adcf
malu <malu@malu.me>
2024-04-17 16:14:17 +0800

    autopush

--- _posts/push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@echo ========================================================
-@echo commit git
-@echo ========================================================
-
-git add . && git commit -am 'update' && git push
-pause
